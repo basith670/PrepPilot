c490d21573ffb7612fb4f38aceb9b74efc050d46
basith670 <basithkdrmf87@gmail.com>
2026-07-05 13:09:06 +0530

    Redesign authentication pages with modern centered UI

--- studyplanner/forms.py
+++ studyplanner/forms.py
@@ -74,17 +74,72 @@ class ResumeForm(forms.ModelForm):
 
 class RegisterForm(UserCreationForm):
 
-    email = forms.EmailField()
+    email = forms.EmailField(
+
+        widget=forms.EmailInput(
+
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter email"
+            }
+
+        )
+
+    )
+
+    username = forms.CharField(
+
+        widget=forms.TextInput(
+
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter username"
+            }
+
+        )
+
+    )
+
+    password1 = forms.CharField(
+
+        widget=forms.PasswordInput(
+
+            attrs={
+                "class": "form-control",
+                "placeholder": "Create password"
+            }
+
+        )
+
+    )
+
+    password2 = forms.CharField(
+
+        widget=forms.PasswordInput(
+
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirm password"
+            }
+
+        )
+
+    )
 
     class Meta:
 
         model = User
 
         fields = [
-            'username',
-            'email',
-            'password1',
-            'password2'
+
+            "username",
+
+            "email",
+
+            "password1",
+
+            "password2"
+
         ]
 
 
